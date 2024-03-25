c993bae428885561345086c7e73cfb845ed02b04
OsmiumD <1653187368@qq.com>
2024-03-25 16:18:23 +0800

    test run

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
-    image = cv2.imread('./sc.png')
+    image = cv2.imread('./sc.jpg')
     print(image.shape)
     resize = cv2.resize(image, (32, 32), interpolation=cv2.INTER_CUBIC)
     print(resize.shape)
--- test_run.py
+++ test_run.py
@@ -5,8 +5,8 @@ import PIL.Image as pilimg
 import numpy
 import numpy as np
 import tensorflow as tf
-import math
-import struct
+from io import StringIO
+from PIL import Image
 import cv2
 
 import os
@@ -24,6 +24,8 @@ from utils.usrp_utils import to_constellation_array
 from config.train_config import BATCH_SIZE
 from config.usrp_config import NORMALIZE_CONSTANT, TEMP_DIRECTORY
 from usrp.pilot import PILOT_SIZE
+from skimage.metrics import peak_signal_noise_ratio
+from utils.usrp_utils import compensate_signal, receive_constellation_tcp
 
 ARCH = 'CCVVCC'
 NUM_SYMBOLS = 512
@@ -57,7 +59,7 @@ if not os.path.exists(TEMP_DIRECTORY):
 if __name__ == '__main__':
     image = cv2.imread('image.png')
     image: numpy.ndarray
-    print(image)
+    # print(image)
     tf_image = tf.convert_to_tensor(image, dtype=tf.float32) / 255.0
     h, w, c = tf_image.shape
     tf_image = tf.reshape(tf_image, (1, h, w, c))
@@ -70,9 +72,31 @@ if __name__ == '__main__':
     )
     tf_image = tf.reshape(tf_image, (-1, 32, 32, c))
     data = encoder_network(tf_image)
+    print(tf.shape(data))
+
     i = data[:, :, 0].numpy().flatten()
     q = data[:, :, 1].numpy().flatten()
     i = np.round(np.clip(i / NORMALIZE_CONSTANT * 32767, -32767, 32767))
     q = np.round(np.clip(q / NORMALIZE_CONSTANT * 32767, -32767, 32767))
-    constellations = to_constellation_array(i, q, i_pilot=True, q_pilot=True)
+    constellations = np.concatenate((i, q))
+    np.savez_compressed(f'{TEMP_DIRECTORY}/data.npz', data=data)
     np.savez_compressed(f'{TEMP_DIRECTORY}/constellations.npz', constellations=constellations)
+    rcv_i = constellations[:512]
+    rcv_q = constellations[512:]
+    rcv_iq = np.c_[rcv_i, rcv_q] * NORMALIZE_CONSTANT / 32767
+    rcv_iq = tf.cast(tf.convert_to_tensor(rcv_iq), tf.float32)
+    rcv_iq = tf.reshape(rcv_iq, (1, -1, 2))
+    proposed_result = decoder_network(rcv_iq)
+    tf.keras.utils.save_img(f'{TEMP_DIRECTORY}/decoded_image.png', imBatchtoImage(proposed_result))
+    cv2.imwrite(f'{TEMP_DIRECTORY}/decoded_image.png', imBatchtoImage(proposed_result).numpy() * 255)
+
+    psnr = peak_signal_noise_ratio(tf_image.numpy(), proposed_result.numpy())
+    print(f'semantic psnr: {psnr}')
+    # print(tf_image.numpy() - proposed_result.numpy())
+
+    img_png = Image.open('./image.png')
+    img_png.save('./temp/img_jpeg.jpeg', quality=90)
+
+    img_jpeg = cv2.imread('./temp/img_jpeg.jpeg')
+    psnr_jpeg = peak_signal_noise_ratio(tf_image.numpy().reshape(32, 32, 3), img_jpeg / 255)
+    print(f'jpeg psnr: {psnr_jpeg}')
